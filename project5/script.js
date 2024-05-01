57054ffda1513a3725b8c686db27faf13595c66f
lecoop <lecoop@syr.edu>
2024-05-01 19:56:41 -0400

    committing suicide fr fr

--- project5/script.js
+++ project5/script.js
@@ -1,1 +1,10 @@
-// making the index_home pic tags buttons//
+function showNav() {
+    document.getElementById("hamicon").style.display = "none"; // Hide hamburger icon
+    document.getElementById("close").style.display = "block"; // Show close icon
+    document.getElementById("navlinks").style.display = "block"; // Show navigation links
+}
+function hideNav() {
+    document.getElementById("hamicon").style.display = "block"; // Show hamburger icon
+    document.getElementById("close").style.display = "none"; // Hide close icon
+    document.getElementById("navlinks").style.display = "none"; // Hide navigation links
+}
