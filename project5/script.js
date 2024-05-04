7642115e36ab650276ade0e0b4331e34aad5ae4a
lecoop <lecoop@syr.edu>
2024-05-03 22:04:37 -0400

    bah

--- project5/script.js
+++ project5/script.js
@@ -8,3 +8,29 @@ function hideNav() {
     document.getElementById("close").style.display = "none"; // Hide close icon
     document.getElementById("navlinks").style.display = "none"; // Hide navigation links
 }
+
+
+function openModal(imageSrc, heading, descriptionId) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImg");
+    var modalHeading = document.getElementById("modalHeading");
+    var modalDescription = document.getElementById("modalDescription");
+
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
+    modalHeading.textContent = heading;
+    modalDescription.textContent = document.getElementById(descriptionId).textContent;
+}
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'block';
+});
+
+document.addEventListener('click', function(event) {
+    var menu = document.getElementById('menu');
+    var overlay = document.getElementById('overlay');
+    
+    if (!menu.contains(event.target) && !overlay.contains(event.target)) {
+        menu.style.display = 'none';
+    }
+});
